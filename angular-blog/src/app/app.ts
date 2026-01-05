105ef9b35ab37d9fdcdc2f51a67d2b94be92f6d7
Lucas Belluzi <lucasdejesusoliveirabelluzi@gmail.com>
2026-01-04 22:26:45 -0300

    feat(components): finishing big-card CSS style and HTML structure for the home page

--- angular-blog/src/app/app.ts
+++ angular-blog/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './pages/home/home';
-import { MenuTitle } from './components/menu-title/menu-title';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuTitle, Home],
+  imports: [RouterOutlet, Home],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
